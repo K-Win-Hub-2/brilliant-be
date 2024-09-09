87a47f4062ea6ddfdfabf86ef01620ab5a17b92c
Kaung Set Hein <127381301+kaung-set107@users.noreply.github.com>
2024-09-09 10:00:55 +0630

    fix

--- app/routes/patientRoutes.js
+++ app/routes/patientRoutes.js
@@ -3,29 +3,51 @@
 const patient = require("../controllers/patientController");
 const { catchError } = require("../lib/errorHandler");
 const verifyToken = require("../lib/verifyToken");
-const upload = require('../lib/fieldUploader').upload;
+const upload = require("../lib/fieldUploader").upload;
 
 module.exports = (app) => {
+  app
+    .route("/api/customers")
+    .post(verifyToken, catchError(patient.createCustomer));
 
-    app.route('/api/patient')
-        .post(upload, verifyToken, catchError(patient.createPatient))
-        .put(upload, verifyToken, catchError(patient.updatePatient))
+  app
+    .route("/api/customers")
+    .get(verifyToken, catchError(patient.listAllCustomers));
 
-    app.route('/api/patient/:id')
-        .get(verifyToken, catchError(patient.getPatient))
-        .delete(verifyToken, catchError(patient.deletePatient))
-        .post(verifyToken, catchError(patient.activatePatient))
+  app
+    .route("/api/customer/:id")
+    .get(verifyToken, catchError(patient.getCustomer));
+  app
+    .route("/api/patient")
+    .post(upload, verifyToken, catchError(patient.createPatient))
+    .put(upload, verifyToken, catchError(patient.updatePatient));
 
-    app.route('/api/patients').get(verifyToken, catchError(patient.listAllPatients))
+  app
+    .route("/api/patient/:id")
+    .get(verifyToken, catchError(patient.getPatient))
+    .delete(verifyToken, catchError(patient.deletePatient))
+    .post(verifyToken, catchError(patient.activatePatient));
 
-    app.route('/api/patients-filter')
-        .get(verifyToken, catchError(patient.filterPatients))
+  app
+    .route("/api/patients")
+    .get(verifyToken, catchError(patient.listAllPatients));
 
-    app.route('/api/patients-search')
-        .post(verifyToken, catchError(patient.searchPatients))
+  app
+    .route("/api/patients-filter")
+    .get(verifyToken, catchError(patient.filterPatients));
 
-    app.route('/api/patients/top-ten').get(verifyToken, catchError(patient.topTenPatients))
-    app.route('/api/patients/excel').post(upload, verifyToken, catchError(patient.excelImportPatient))
+  app
+    .route("/api/patients-search")
+    .post(verifyToken, catchError(patient.searchPatients));
 
-    app.route('/api/patients/history-and-patient/:id').get(verifyToken, catchError(patient.getHistoryAndPhysicalExamination))
+  app
+    .route("/api/patients/top-ten")
+    .get(verifyToken, catchError(patient.topTenPatients));
+  app
+    .route("/api/patients/excel")
+    .post(upload, verifyToken, catchError(patient.excelImportPatient));
+
+  app
+    .route("/api/patients/history-and-patient/:id")
+    .get(verifyToken, catchError(patient.getHistoryAndPhysicalExamination));
 };
