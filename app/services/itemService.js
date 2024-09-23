7d327234c48f6cb283374c28616afb7d7817e3f7
Kaung Set Hein <127381301+kaung-set107@users.noreply.github.com>
2024-09-23 15:54:56 +0630

    fix

--- app/services/itemService.js
+++ app/services/itemService.js
@@ -1,68 +1,107 @@
-"use strict"
+"use strict";
 
-const { paginationHelper } = require("../helper/paginationHelper")
-const items = require("../models/items")
-const { createItemAndShareHolder } = require("./itemAndShareHolderService")
+const { paginationHelper } = require("../helper/paginationHelper");
+const items = require("../models/items");
+const { createItemAndShareHolder } = require("./itemAndShareHolderService");
 
 exports.getAllItems = async (datas) => {
-    try{
-       let { s, c, limit, offset, sort } = datas
-       let query = { isDeleted: false }
-       let sortByAscending = {id: 1}
-       s ? query.relatedSuperCategory = s : ""
-       c ? query.code= c : ""
-       if (sort) sortByAscending = { id: -1 }
-       let count = await items.find(query).count()
-       let paginationHelpers = await paginationHelper(count, offset, limit) 
-       let result = await items.find(query).populate("relatedSuperCategory relatedItemTitle").limit(paginationHelpers.limit).skip(paginationHelpers.skip).sort(sortByAscending).exec()
-        return { data: result, meta_data: paginationHelpers}; 
-    }catch(err){
-        console.log("Error is", err.message)
-    }
-    
-}
+  try {
+    let { s, c, limit, offset, sort } = datas;
+    //   let query = { isDeleted: false };
+    let query = { isDeleted: false },
+      regexKeyword;
+    let sortByAscending = { id: 1 };
+    s ? (query.relatedSuperCategory = s) : "";
+    c ? (query.code = c) : "";
+    if (sort) sortByAscending = { id: -1 };
+    let count = await items.find(query).count();
+    let paginationHelpers = await paginationHelper(count, offset, limit);
+    let result = await items
+      .find(query)
+      .populate("relatedSuperCategory")
+      .populate({
+        path: "relatedShareHolder",
+        populate: {
+          path: "shareholder_id",
+          model: "shareholders",
+        },
+      })
+      .populate({
+        path: "relatedItemTitle",
+
+        populate: {
+          path: "relatedSubCategory",
+          model: "SubCategories",
+        },
+      })
+      .populate({
+        path: "relatedItemTitle",
+        populate: {
+          path: "relatedBrand",
+          model: "Brands",
+        },
+      })
+      .limit(paginationHelpers.limit)
+      .skip(paginationHelpers.skip)
+      .sort(sortByAscending)
+      .exec();
+    return { data: result, meta_data: paginationHelpers };
+  } catch (err) {
+    console.log("Error is", err.message);
+  }
+};
 
 exports.createItems = async (datas) => {
-    try{
-       let result = await items.create(datas)
-       //if there are too many shareholder then change in this like loop through the shareholder array
-       const itemShareHolderResult = await createItemAndShareHolder({
-        relatedShareHolder: datas.relatedShareHolder.shareholder_id,
-        percent: datas.relatedShareHolder.percent,
-        relatedItem: result._id
-    })
-       return result; 
-    }
-    catch(err){
-        console.log("Error is", err.message)
-    }
-}
+  try {
+    let result = await items.create(datas);
+    //if there are too many shareholder then change in this like loop through the shareholder array
+    const itemShareHolderResult = await createItemAndShareHolder({
+      relatedShareHolder: datas.relatedShareHolder.shareholder_id,
+      percent: datas.relatedShareHolder.percent,
+      relatedItem: result._id,
+    });
+    return result;
+  } catch (err) {
+    console.log("Error is", err.message);
+  }
+};
 
 exports.getItemById = async (id) => {
-    try{
-       let result = await items.findById(id).populate("relatedSuperCategory relatedItemTitle")
-        return result; 
-    }catch(err){
-        console.log("Error is", err.message)
-    }
-    
-}
+  try {
+    let result = await items
+      .findById(id)
+      .populate("relatedSuperCategory relatedItemTitle")
+      .populate({
+        path: "relatedShareHolder",
+        populate: {
+          path: "shareholder_id",
+          model: "shareholders",
+        },
+      });
+    return result;
+  } catch (err) {
+    console.log("Error is", err.message);
+  }
+};
 
 exports.updateItem = async (id, datas) => {
-    try{
-       let result = await items.findByIdAndUpdate(id, datas, { new: true })
-       return result; 
-    }catch(err){
-        console.log("Error is", err.message)
-    }
-}
+  try {
+    let result = await items.findByIdAndUpdate(id, datas, { new: true });
+    return result;
+  } catch (err) {
+    console.log("Error is", err.message);
+  }
+};
 
 exports.deleteItem = async (id) => {
-    try{
-       let result = await items.findByIdAndUpdate(id, {isDeleted: true}, { new: true })
-       return result; 
-    }catch(err){
-        console.log("Error is", err.message)
-    }
-}
-
+  try {
+    let result = await items.findByIdAndUpdate(
+      id,
+      { isDeleted: true },
+      { new: true }
+    );
+    return result;
+  } catch (err) {
+    console.log("Error is", err.message);
+  }
+};
